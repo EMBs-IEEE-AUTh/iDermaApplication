3f14f95b17e3bab1f45ae2190c0346697e003f54
ioanna <ioannavrachni@gmail.com>
2024-08-14 15:56:06 +0300

    Analyze and Results Screens.

--- lib/screens/authentication/login_screen.dart
+++ lib/screens/authentication/login_screen.dart
@@ -1,23 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:okeyy/components/my_button.dart';
+import 'package:okeyy/components/square_tile.dart';
+import 'package:okeyy/screens/Sign_Up.dart';
+import 'package:okeyy/screens/login_screen.dart';
+import 'package:okeyy/services/auth_service.dart';
 
-void main() => runApp(MyApp());
+class LoginPagesScreen extends StatefulWidget {
+  final Function()? onTap;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const LoginPagesScreen({super.key, this.onTap});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginScreen(),
-    );
-  }
+  State<LoginPagesScreen> createState() => _LoginPagesScreenState();
 }
 
-// TODO: Maybe I should change the state of the class to a stateful widget.
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
+class _LoginPagesScreenState extends State<LoginPagesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +24,7 @@ class LoginScreen extends StatelessWidget {
         builder: (context, constraints) {
           return Column(
             children: [
+              //logo container
               Container(
                 width: double.infinity,
                 padding: const EdgeInsets.only(top: 70.0, bottom: 6),
@@ -43,6 +42,8 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
               ),
+
+              //Login text
               Expanded(
                 child: SingleChildScrollView(
                   padding: const EdgeInsets.symmetric(
@@ -69,47 +70,73 @@ class LoginScreen extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 30),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton.icon(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 16),
-                            backgroundColor: const Color(0xFF4285F4),
-                            foregroundColor: Colors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 20.0),
-                            elevation: 0, // Remove shadow
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100.0),
-                              side: const BorderSide(
-                                  color: Color.fromRGBO(66, 133, 244, 1)),
-                            ),
-                          ),
-                          icon: const Icon(Icons.login, color: Colors.white),
-                          label: const Text('Sign in with Google'),
-                        ),
+
+                      //Google Sign In
+                      // SizedBox(
+                      //   width: double.infinity,
+                      //   child: ElevatedButton.icon(
+                      //     onPressed: () {},
+                      //     style: ElevatedButton.styleFrom(
+                      //       textStyle: const TextStyle(fontSize: 16),
+                      //       backgroundColor: const Color(0xFF4285F4),
+                      //       foregroundColor: Colors.white,
+                      //       padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      //       elevation: 0, // Remove shadow
+                      //       shape: RoundedRectangleBorder(
+                      //         borderRadius: BorderRadius.circular(100.0),
+                      //         side: const BorderSide(
+                      //             color: Color.fromRGBO(66, 133, 244, 1)),
+                      //       ),
+                      //     ),
+                      //     icon: const Icon(Icons.login, color: Colors.white),
+                      //     label: const Text('Sign in with Google'),
+                      //   ),
+                      // ),
+                      // const SizedBox(height: 20),
+
+                      //Apple ID Sing in
+                      // SizedBox(
+                      //   width: double.infinity,
+                      //   child: ElevatedButton.icon(
+                      //     onPressed: () {},
+                      //     style: ElevatedButton.styleFrom(
+                      //       textStyle: const TextStyle(fontSize: 16),
+                      //       backgroundColor: Colors.black,
+                      //       foregroundColor: Colors.white,
+                      //       padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      //       elevation: 0, // Remove shadow
+                      //       shape: RoundedRectangleBorder(
+                      //         borderRadius: BorderRadius.circular(100.0),
+                      //         side: const BorderSide(color: Color(0xFFD4D4D4)),
+                      //       ),
+                      //     ),
+                      //     icon: const Icon(Icons.apple, color: Colors.white),
+                      //     label: const Text('Sign in with Apple'),
+                      //   ),
+                      // ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          //google button
+                          SquareTile(
+                              imagePath: 'lib/images/google.png',
+                              buttonText: 'Sign In with google',
+                              onTap: () => AuthService().signInWithGoogle(),
+                              width: 400.00),
+
+                          const SizedBox(height: 10),
+
+                          //apple button
+                          SquareTile(
+                            imagePath: 'lib/images/apple.png',
+                            buttonText: 'Sign In with Apple ID',
+                            onTap: () => AuthService().signInWithGoogle(),
+                          )
+                        ],
                       ),
                       const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton.icon(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 16),
-                            backgroundColor: Colors.black,
-                            foregroundColor: Colors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 20.0),
-                            elevation: 0, // Remove shadow
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100.0),
-                              side: const BorderSide(color: Color(0xFFD4D4D4)),
-                            ),
-                          ),
-                          icon: const Icon(Icons.apple, color: Colors.white),
-                          label: const Text('Sign in with Apple'),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
+
+                      //Facebook Sign In
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton.icon(
@@ -130,17 +157,19 @@ class LoginScreen extends StatelessWidget {
                           label: const Text('Sign in with Facebook'),
                         ),
                       ),
+
+                      //Email Sign In
                       const SizedBox(height: 20),
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton.icon(
                           onPressed: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //     builder: (context) => const LoginWithEmail(),
-                            //   ),
-                            // );
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
                           },
                           style: ElevatedButton.styleFrom(
                             textStyle: const TextStyle(fontSize: 16),
@@ -157,6 +186,7 @@ class LoginScreen extends StatelessWidget {
                           icon: const SizedBox.shrink(),
                         ),
                       ),
+
                       const SizedBox(height: 20),
                       Center(
                         child: Text(
@@ -167,10 +197,17 @@ class LoginScreen extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 10),
+
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton.icon(
-                          onPressed: () {},
+                          onPressed: () {
+                            // Navigate to a new page
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SignUp()),
+                            );
+                          },
                           style: ElevatedButton.styleFrom(
                             textStyle: const TextStyle(fontSize: 16),
                             backgroundColor: const Color(0xFF2C3D8F),
@@ -183,9 +220,11 @@ class LoginScreen extends StatelessWidget {
                             ),
                           ),
                           label: const Text('Create Account'),
-                          icon: const Icon(Icons.account_circle, color: Colors.white),
+                          icon: const Icon(Icons.account_circle,
+                              color: Colors.white),
                         ),
                       ),
+
                       const SizedBox(height: 20),
                     ],
                   ),
---
+++ lib/screens/authentication/sign_up_screen.dart
@@ -0,0 +1,456 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:iDermaApplication/components/account_created.dart';
+import 'package:okeyy/components/account_created.dart';
+import 'package:okeyy/components/custom_button.dart';
+import 'package:okeyy/components/my_button.dart';
+import 'package:okeyy/components/square_tile.dart';
+import 'package:okeyy/screens/home_page_screen.dart';
+import 'package:okeyy/services/auth_service.dart'; // Import the new page
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final List<String> placeholderTexts = [
+    'First Name',
+    'Last Name',
+    'Email Address',
+    'Password',
+    'Confirm password',
+  ];
+
+  Color createAccountButtonColor = Color.fromRGBO(44, 61, 143, 1.0);
+  Color googleButtonColor = Color.fromRGBO(246, 246, 248, 1);
+  Color facebookButtonColor = Color.fromRGBO(44, 61, 143, 1.0);
+
+  bool isChecked = false;
+  bool isFormValid = false;
+
+//---------------------------------------------------CONTROLLERS---------------------------------------------------------
+
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+//---------------------------------------------------ACTION_LISTENERS----------------------------------------------------------
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController.addListener(_checkFormValidity);
+    _lastNameController.addListener(_checkFormValidity);
+    _emailController.addListener(_checkFormValidity);
+    _passwordController.addListener(_checkFormValidity);
+    _confirmPasswordController.addListener(_checkFormValidity);
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+//---------------------------------------------------VERIFICATION_FOR_CONTROLLERS---------------------------------------------------------
+
+  void _checkFormValidity() {
+    setState(() {
+      isFormValid = _firstNameController.text.isNotEmpty &&
+          _lastNameController.text.isNotEmpty &&
+          _emailController.text.isNotEmpty &&
+          _passwordController.text.isNotEmpty &&
+          _confirmPasswordController.text.isNotEmpty &&
+          isChecked;
+    });
+  }
+
+//---------------------------------------------------CREATE_ACCOUNT_BOXES---------------------------------------------------------
+
+  void _createAccount() {
+    if (isFormValid) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => AccountCreatedPage()),
+      );
+    }
+  }
+
+  //------------------------------------------------Show_Error_Message------------------------------------------------------------
+  void showErrorMessage(String message) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(message),
+          );
+        });
+  }
+
+  //------------------------------------------------Sign_User_Up------------------------------------------------------------------
+  void signUserUp() async {
+    // Show loading circle
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return const Center(child: CircularProgressIndicator());
+      },
+    );
+
+    try {
+      // Check if password and confirm password are the same
+      if (_passwordController.text == _confirmPasswordController.text) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+        // Navigate to Home Screen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePageScreen()),
+        );
+      } else {
+        // Show error message if passwords don't match
+        showErrorMessage("Passwords don't match");
+      }
+    } on FirebaseAuthException catch (e) {
+      // Handle errors
+      Navigator.pop(context); // Close the loading spinner
+      if (e.code == 'email-already-in-use') {
+        showErrorMessage("This email is already in use.");
+      } else if (e.code == 'weak-password') {
+        showErrorMessage("Your password is too weak.");
+      } else {
+        showErrorMessage("Something went wrong. Please try again.");
+      }
+    } catch (e) {
+      Navigator.pop(
+          context); // Close the loading spinner in case of unexpected errors
+      showErrorMessage("An unexpected error occurred.");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              buildInitialBox(),
+              SizedBox(height: 40),
+              Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Color.fromRGBO(44, 61, 143, 1.0),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Aligns text and text box to the left
+                children: [
+                  const Text(
+                    'First Name',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromRGBO(44, 61, 143, 1.0),
+                    ), // You can adjust the font size if needed
+                  ),
+                  const SizedBox(
+                      height:
+                          4), // Add a small space between the label and the text box
+                  buildTextBox(
+                    controller: _firstNameController,
+                    placeholderText: placeholderTexts[0],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Aligns text and text box to the left
+                children: [
+                  const Text(
+                    'Last Name',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromRGBO(44, 61, 143, 1.0),
+                    ), // You can adjust the font size if needed
+                  ),
+                  const SizedBox(
+                      height:
+                          4), // Add a small space between the label and the text box
+                  buildTextBox(
+                    controller: _lastNameController,
+                    placeholderText: placeholderTexts[1],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Aligns text and text box to the left
+                children: [
+                  const Text(
+                    'Email Address',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromRGBO(44, 61, 143, 1.0),
+                    ), // You can adjust the font size if needed
+                  ),
+                  const SizedBox(
+                      height:
+                          4), // Add a small space between the label and the text box
+                  buildTextBox(
+                    controller: _emailController,
+                    placeholderText: placeholderTexts[2],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Aligns text and text box to the left
+                children: [
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromRGBO(44, 61, 143, 1.0),
+                    ), // You can adjust the font size if needed
+                  ),
+                  const SizedBox(
+                      height:
+                          4), // Add a small space between the label and the text box
+                  buildTextBox(
+                    controller: _passwordController,
+                    placeholderText: placeholderTexts[3],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment
+                    .start, // Aligns text and text box to the left
+                children: [
+                  const Text(
+                    'Confirm Password',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Color.fromRGBO(44, 61, 143, 1.0),
+                    ), // You can adjust the font size if needed
+                  ),
+                  const SizedBox(
+                      height:
+                          4), // Add a small space between the label and the text box
+                  buildTextBox(
+                    controller: _confirmPasswordController,
+                    placeholderText: placeholderTexts[4],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    value: isChecked,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        isChecked = value ?? false;
+                        _checkFormValidity();
+                      });
+                    },
+                  ),
+                  Text(
+                    'I agree to the terms and conditions',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              MyButton(onTap: signUserUp, text: 'Sign Up'),
+              SizedBox(height: 8),
+              Text(
+                'or',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromRGBO(44, 104, 226, 1),
+                ),
+              ),
+              SizedBox(height: 8),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //google button
+                  SquareTile(
+                      imagePath: 'lib/images/google.png',
+                      buttonText: 'Sign In with google',
+                      onTap: () => AuthService().signInWithGoogle(),
+                      width: 300.00),
+
+                  const SizedBox(height: 10),
+
+                  //apple button
+                  SquareTile(
+                      imagePath: 'lib/images/apple.png',
+                      buttonText: 'Sign In with Apple ID',
+                      onTap: () => AuthService().signInWithGoogle(),
+                      width: 300.00)
+                ],
+              ),
+              SizedBox(height: 50),
+              buildInitialBoxLogo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+//---------------------------------------------------UPPER_LOG0---------------------------------------------------------
+
+  Widget buildInitialBox() {
+    return Container(
+      color: Color.fromRGBO(44, 61, 143, 1.0),
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 70.0),
+      child: Container(
+        width: 400,
+        height: 82,
+        color: Color.fromRGBO(44, 61, 143, 1.0),
+        child: Center(
+          child: Text(
+            'iDerma',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+//---------------------------------------------------TEXT_BOXES---------------------------------------------------------
+
+  Widget buildTextBox(
+      {required TextEditingController controller,
+      required String placeholderText}) {
+    return Container(
+      width: 290.15,
+      height: 42,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(205, 222, 255, 1.0),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          SizedBox(width: 18),
+          Expanded(
+            child: TextFormField(
+              controller: controller,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+                fontWeight: FontWeight.normal,
+              ),
+              textAlignVertical: TextAlignVertical.center,
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: placeholderText,
+                hintStyle: TextStyle(
+                  color: Color.fromRGBO(44, 61, 143, 1.0),
+                ),
+                contentPadding: EdgeInsets.zero,
+                isDense: true,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+//---------------------------------------------------CREATE_ACCOUNT_BUTTON--------------------------------------------------------
+
+  Widget buildCircularButton() {
+    return GestureDetector(
+      onTap: isFormValid ? _createAccount : null,
+      onTapDown: (_) {
+        if (isFormValid) {
+          setState(() {
+            createAccountButtonColor = Color.fromRGBO(205, 222, 255, 1.0);
+          });
+        }
+      },
+      onTapUp: (_) {
+        if (isFormValid) {
+          setState(() {
+            createAccountButtonColor = Color.fromRGBO(44, 61, 143, 1.0);
+          });
+        }
+      },
+      child: CustomButton(
+        buttonText: 'Create Account',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePageScreen()),
+          );
+          // Define your button action here
+        },
+        isFormValid: true, // or false depending on form validation
+        createAccountButtonColor: Color(0xFF2C3D8F), // Active button color
+      ),
+    );
+  }
+
+//---------------------------------------------------LOWER_LOG0---------------------------------------------------------
+
+  Widget buildInitialBoxLogo() {
+    return Container(
+      color: Color.fromRGBO(44, 61, 143, 1.0),
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 10.0),
+      child: Container(
+        width: 60,
+        height: 52,
+        color: Color.fromRGBO(44, 61, 143, 1.0),
+        child: Center(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 0.5),
+              child: Text(
+                'EMB AUTH',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
